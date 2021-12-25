c6bbbdbd7cf319b644dd914a11b3279211d0a10e
Cody Mims <mims@hey.com>
2021-12-24 23:50:57 -0600

    Update favicon

--- textreverser.js
+++ textreverser.js
@@ -1,32 +1,35 @@
+const dangerClass = "red";
+const reversedHeading = ".esrever ot tnaw uoy txet eht retnE";
+const s = "s";
 let input = document.getElementById("input");
 let characterNotice = document.getElementById("character-notice");
 let characterCount = document.getElementById("character-count");
 const maxCharacters = parseInt(characterCount.textContent);
-let s = document.getElementById("s");
+let sSpan = document.getElementById("s");
 let output = document.getElementById("output");
 input.addEventListener("input", function (event) {
     let count = maxCharacters - event.target.value.length;
     characterCount.textContent = count;
     if (count <= 10) {
-        characterNotice.classList.add("red");
+        characterNotice.classList.add(dangerClass);
     } else {
-        characterNotice.classList.remove("red");
+        characterNotice.classList.remove(dangerClass);
     }
     if (count === 1) {
-        s.textContent = "";
+        sSpan.textContent = "";
     } else {
-        s.textContent = "s";
+        sSpan.textContent = s;
     }
     if (count === maxCharacters) {
-        output.textContent = ".esrever ot tnaw uoy txet eht retnE";
+        output.textContent = reversedHeading;
     } else {
         output.textContent = event.target.value.split("").reverse().join("");
     }
 });
 document.getElementById("clear").addEventListener("click", function () {
     input.value = "";
-    characterNotice.classList.remove("red");
+    characterNotice.classList.remove(dangerClass);
     characterCount.textContent = maxCharacters;
-    s.textContent = "s";
-    output.textContent = ".esrever ot tnaw uoy txet eht retnE";
+    sSpan.textContent = s;
+    output.textContent = reversedHeading;
 });
